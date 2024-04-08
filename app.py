45115ab365222197a50083489eebe61a7a36ec26
DANG NGOC CHIEN <chen@asterone.co.jp>
2024-04-08 14:14:37 +0900

    新データ

--- app.py
+++ app.py
@@ -18,29 +18,42 @@ random.seed()  # Initialize the random number generator
 
 def generate_data():
     raspberrypiData = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    raspberrypiData.connect(('192.168.11.101', 5000))
+    raspberrypiData.connect(('192.168.11.90', 5000))
     data_decode = (raspberrypiData.recv(1024).decode('utf-8'))
     return data_decode
 def timer():
     t = time.time()  # UNIX時間（1970/01/01 00:00:00からの経過時刻）を取得
     local_time = time.localtime(t)  # ローカル時刻をtime.struct_time型として取得
-    ConnectionAbortedError
     asc_time = time.asctime(local_time)  # 上のlocal_timeを文字列表現に変換
     dt = datetime.now().strptime(asc_time, '%a %b %d %H:%M:%S %Y')
     return dt
 
 # def generate_data1():
 #     raspberrypiData = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     raspberrypiData.connect(('192.168.11.75', 5000))
+#     raspberrypiData.connect(('192.168.11.75', 6000))
 #     data_decode = (raspberrypiData.recv(1024).decode('utf-8'))
 #     return data_decode
+
+@application.route('/')
+def index():
+            data_decode = generate_data()
+            split = re.split('\s+', data_decode)
+            temperature = float(split[0])
+            humidity = float(split[1])
+            pressure = float(split[2])
+            sensor = [
+                {
+                    "temperature": temperature,
+                    "huminity": humidity,
+                    "pressure": pressure
+                }
+            ]
+            return render_template('index.html', text=sensor)   
 
 @application.route('/data')
 def data():
     while True:
         sensor_data = generate_data()
-        # sensor_data1 = generate_data1()
-        # print("sensor_data1", sensor_data1)
         split = re.split('\s+', sensor_data)
         print("split", split)
         temperature = float(split[0])
@@ -54,26 +67,6 @@ def data():
             }
         ]
         return jsonify(data)
-
-@application.route('/')
-def index():
-    while True:
-            raspberrypiData = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            raspberrypiData.connect(('192.168.11.101', 5000))
-            data_decode = (raspberrypiData.recv(1024).decode('utf-8'))
-            split = re.split('\s+', data_decode)
-            temperature = float(split[0])
-            humidity = float(split[1])
-            pressure = float(split[2])
-            
-            sensor = [
-                {
-                    "temperature": temperature,
-                    "huminity": humidity,
-                    "pressure": pressure
-                }
-            ]
-            return render_template('index.html', text=sensor)
 
 @application.route('/chart-data')
 def chart_data():
@@ -135,6 +128,7 @@ def chart_data2():
     response.headers["Cache-Control"] = "no-cache"
     response.headers["X-Accel-Buffering"] = "no"
     return response
+
 
 if __name__ == "__main__":
     application.run(debug=True, host="localhost", port=3000)
